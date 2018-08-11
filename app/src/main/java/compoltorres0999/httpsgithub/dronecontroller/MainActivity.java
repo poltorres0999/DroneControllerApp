f30245117452c0cb125f023374f48dc512bd7b01
poltorres0999 <eichgammer@gmail.com>
2018-08-11 21:24:23 +0200

    Added validation for ipAddres and Port

--- app/src/main/java/compoltorres0999/httpsgithub/dronecontroller/MainActivity.java
+++ app/src/main/java/compoltorres0999/httpsgithub/dronecontroller/MainActivity.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String IP_ADDRESS = "IP_ADDRESS";
     public static final String PORT = "PORT";
+    private static final Pattern PATTERN = Pattern.compile(
+            "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
 
     private EditText ipAddress;
     private EditText commandPort;
+
 
 
     @Override
@@ -29,11 +34,37 @@ public class MainActivity extends AppCompatActivity {
     public void startConnection(View view) {
         Intent startClient = new Intent(this, ClientActivity.class);
 
-        String Ip = ipAddress.getText().toString();
-        int port = Integer.parseInt(commandPort.getText().toString());
-        startClient.putExtra(IP_ADDRESS, Ip);
-        startClient.putExtra(PORT, port);
-        startActivity(startClient);
+        if (this.checkParams()) {
+            String Ip = ipAddress.getText().toString();
+            int port = Integer.parseInt(commandPort.getText().toString());
+            startClient.putExtra(IP_ADDRESS, Ip);
+            startClient.putExtra(PORT, port);
+            startActivity(startClient);
+        }
+
     }
+
+    private boolean checkParams () {
+
+        boolean correct = true;
+
+        if (ipAddress.getText().toString().isEmpty()) {
+            ipAddress.setError("Ip address can not be blank!");
+            correct = false;
+
+        } else  if (!PATTERN.matcher(ipAddress.getText().toString()).matches()) {
+            ipAddress.setError("Ip Address must have ip structure");
+            correct = false;
+        }
+
+        if (commandPort.getText().toString().isEmpty()) {
+            commandPort.setError("Port can not be blank!");
+            correct = false;
+        }
+
+        return correct;
+
+    }
+
 
 }
